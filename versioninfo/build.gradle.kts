86d579add26f49d6152051ac138b374347cc6a73
Sascha Peilicke <sascha@peilicke.de>
2025-01-30 16:02:52 +0100

    Release version 2.3.1
    Signed-off-by: Sascha Peilicke <sascha@peilicke.de>

--- versioninfo/build.gradle.kts
+++ versioninfo/build.gradle.kts
@@ -52,7 +52,7 @@ publishing {
         register<MavenPublication>("mavenAndroid") {
             artifactId = "android-versioninfo"
             groupId = "de.peilicke.sascha"
-            version = "2.3.0"
+            version = "2.3.1"
 
             afterEvaluate {
                 from(components["release"])
